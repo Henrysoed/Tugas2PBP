79f5ccc1222fe87b86766991c76a7f1b98654dfe
Henrysoed <henry.soedibjo@gmail.com>
2023-09-17 11:11:55 +0700

    Tugas 3

--- Tugas2PBP/settings.py
+++ Tugas2PBP/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Tugas2PBP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views.py
+++ main/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
 
 def show_main(request):
+    items = Item.objects.all
+
     context = {
         'name': 'Henry Soedibjo',
         'class': 'PBP A',
+        'items': items,
         'amount': '100',
         'description': 'henrysoed Investment Portofolio Inventory for individu task 2 PBP'
     }
 
     return render(request, "main.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
